bbda57fb3bc0c3ae7f421dcf4ffd627e31021e1c
hunzzzzz <hun819.st@gmail.com>
2025-08-25 22:06:53 +0900

    first commit

---
+++ src/main/java/com/moira/moirabackend/MoiraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.moira.moirabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoiraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoiraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moira/moirabackend/MoiraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moira.moirabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoiraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
